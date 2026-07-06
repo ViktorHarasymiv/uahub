ef332756591df0bc3b515c6ec718af6f92eb622b
Viktor <victorharasymiv97@gmail.com>
2026-07-06 19:38:30 +0200

    DEBUG PROXY v1

--- frontend/app/(auth routes)/(profile)/layout.tsx
+++ frontend/app/(auth routes)/(profile)/layout.tsx
@@ -2,21 +2,12 @@
 
 import AsideNav from "@/app/components/Profile/AsideNav/AsideNav";
 import { useWindowWidth } from "@/app/hook/useWindowWidth";
-import { useRouter } from "next/navigation";
-
-import { useEffect } from "react";
 export default function ProfileLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const router = useRouter();
-
   const width = useWindowWidth();
-
-  useEffect(() => {
-    router.refresh();
-  }, [router]);
 
   return (
     <div className="container rotate_page">
