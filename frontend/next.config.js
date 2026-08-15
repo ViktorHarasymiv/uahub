4c6d060d6b481946b18c45f411a57f2b441addf9
Viktor <victorharasymiv97@gmail.com>
2026-08-15 20:25:48 +0200

    New commit 19:44

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -17,14 +17,14 @@ const nextConfig = {
     ],
   },
 
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "https://uahub-17tb.onrender.com/api/:path*",
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/api/:path*",
+  //       destination: "https://uahub-17tb.onrender.com/api/:path*",
+  //     },
+  //   ];
+  // },
 };
 
 module.exports = nextConfig;
