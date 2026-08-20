9d9082b1936c6566d1258e5a54796159507efd37
Viktor <victorharasymiv97@gmail.com>
2026-08-21 01:04:47 +0200

    New update 21.08 0:1

--- frontend/app/api/listing/[id]/view/route.ts
+++ frontend/app/api/listing/[id]/view/route.ts
@@ -2,12 +2,13 @@ import { NextRequest, NextResponse } from "next/server";
 import { api } from "../../../api";
 import { AxiosError } from "axios";
 
-export async function PATCH(
-  req: NextRequest,
-  { params }: { params: { id: string } },
-) {
+export async function PATCH(req: NextRequest, context: any) {
   try {
-    const apiRes = await api.patch(`/listing/${params.id}/view`);
+    const { id } = await context.params; // ← важливо: await
+
+    const body = await req.json();
+
+    const apiRes = await api.patch(`/listing/${id}/view`, body);
 
     return NextResponse.json(apiRes.data, { status: apiRes.status });
   } catch (err) {
